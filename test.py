9e77a4f90b64c08ad634eaf5d86864267c8fd334
Abhishek0530 <abhiy1023@gmail.com>
2025-05-07 18:42:09 +0530

    finding the location of library

---
+++ test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def main():
+    # Print a statement
+    print("Hello, World!")
+    
+    # Print the location of the pandas library
+    print("Pandas is installed at:", pd.__file__)
+
+# Call the main function
+if __name__ == "__main__":
+    main()
